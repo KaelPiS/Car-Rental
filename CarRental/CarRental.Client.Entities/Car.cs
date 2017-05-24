28713c7ff7c9c8d90b7f6334e461a347146d34f3
Kael <piskael03@gmail.com>
2017-05-24 13:03:26 -0400

    Initial Project
    Create Project structure, Car class and some Core Classes and Interfaces

---
+++ CarRental/CarRental.Client.Entities/Car.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Client.Entities
+{
+    public class Car
+    {
+        private string _CarID;
+
+        public string CarID
+        {
+            get
+            {
+                return _CarID;
+            }
+
+            set
+            {
+                _CarID = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _Description;
+            }
+
+            set
+            {
+                _Description = value;
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                return _Color;
+            }
+
+            set
+            {
+                _Color = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return _Year;
+            }
+
+            set
+            {
+                _Year = value;
+            }
+        }
+
+        public decimal RentalPrice
+        {
+            get
+            {
+                return _RentalPrice;
+            }
+
+            set
+            {
+                _RentalPrice = value;
+            }
+        }
+
+        public bool CurrentlyRented
+        {
+            get
+            {
+                return _CurrentlyRented;
+            }
+
+            set
+            {
+                _CurrentlyRented = value;
+            }
+        }
+
+        private string _Description;
+
+        private string _Color;
+
+        private int _Year;
+
+        private decimal _RentalPrice;
+
+        private bool _CurrentlyRented;
+    }
+}
---
+++ CarRental/Core.Common/Cores/EntitesBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Common.Cores
+{
+    [DataContract]
+    public class EntitesBase : IExtensibleDataObject
+    {
+        public ExtensionDataObject ExtensionData { get; set; }
+      
+    }
+}
