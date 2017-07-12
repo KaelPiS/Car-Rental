a25a5c61b249d5d1f609bc3ff90866be44a6d0bd
Khang Tran <piskael03@gmail.com>
2017-07-12 16:12:28 -0400

    Adding Entity Framwork and its classes

---
+++ CarRental/Rental.Data/CarRentalContext.cs
@@ -0,0 +1,39 @@
+﻿using CarRental.Business.Entities;
+using Core.Common.Contract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CarRental.Data
+{
+    public class CarRentalContext : DbContext
+    {
+        public CarRentalContext() : base("name=CarRental")
+        {
+            Database.SetInitializer<CarRentalContext>(null); //Tell the database to use this context class
+        }
+
+        public DbSet<Account> AccountSet { get; set; }
+
+        public DbSet<Car> CarSet { get; set; }
+
+        public DbSet<Rental> RentalSet { get; set; }
+        
+        public DbSet<Reservation> ReservationSet { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); //Remove default Name Convention
+
+            modelBuilder.Ignore<ExtensionDataObject>();
+            modelBuilder.Ignore<IIdentifiableEntity>();
+
+        }
+
+    }
+}
+
